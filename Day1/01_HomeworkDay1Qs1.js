d55efc849570b54804a324c3088155ba93ccd164
SowmyaParth-25 <sowmyaparthasarathy15@gmail.com>
2025-01-29 17:42:29 -0500

    Week1-Day1

---
+++ Day1/01_HomeworkDay1Qs1.js
@@ -0,0 +1,12 @@
+const browserVersion="Chrome";
+
+function getBrowserVersion(){
+    if(browserVersion.startsWith="Chrome"){
+     
+        let browserVersion='132'
+        console.log("Browser version is:",browserVersion)
+    }
+    
+    console.log("Browser Version is:",browserVersion)
+}
+getBrowserVersion()
